a72c601ac630ef9c4023d335de524f040e58e2c6
Narcisa <rusnarcisa16@gmail.com>
2022-04-19 17:49:16 +0300

    event-component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent implements OnInit {
+  title = "Main content";
+  subTitlu = "Titlu content";
+  titlePara = "Paragraph1";
+  titleMouse = "Mouseee";
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.title = "Second title";
+    }, 2000);
   }
 
+  schimbaTitlu() {
+    this.title = "Third title";
+  }
+
+  onMouseOver() {
+    this.subTitlu = "Titlu content2";
+  }
+
+  onMouseEnter() {
+    this.titlePara = "Paragraph22222";
+  }
+
+  onMouseOut() {
+    this.titleMouse = "Yesssss";  }
 }
